a5bfef3e4bfe39416c4016b6235fee0559baddaf
Daria Vusik <dariavusik@gmail.com>
2022-07-03 00:51:45 +0300

    Move canvas code to separate file

---
+++ js/gameboard.js
@@ -0,0 +1,65 @@
+'use strict';
+
+class GameBoard {
+    constructor(canvas, context, boardWidthCells, boardHeightCells, cellSizePx, strokeWidthPx) {
+        this.canvas = canvas;
+        this.context = context;
+        this.maxWidth = boardWidthCells;
+        this.maxHeight = boardHeightCells;
+        this.cellSizePx = cellSizePx;
+        this.strokeWidthPx = strokeWidthPx;
+
+        canvas.width = this.maxWidth * this.cellSizePx + this.strokeWidthPx;
+        canvas.height = this.maxHeight * this.cellSizePx + this.strokeWidthPx;
+
+        let data = `<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"> \
+        <defs> \
+            <pattern id="grid" width="${this.cellSizePx}" height="${this.cellSizePx}" patternUnits="userSpaceOnUse"> \
+                <path d="M ${this.cellSizePx} 0 L 0 0 0 ${this.cellSizePx}" fill="none" stroke="gray" stroke-width="${this.strokeWidthPx}" /> \
+            </pattern> \
+        </defs> \
+        <rect width="100%" height="100%" fill="url(#grid)" /> \
+        </svg>`;
+
+        let DOMURL = window.URL || window.webkitURL || window;
+
+        this.image = new Image();
+        let svg = new Blob([data], { type: 'image/svg+xml;charset=utf-8' });
+        let url = DOMURL.createObjectURL(svg);
+
+        this.image.onload = function () {
+            this.context.drawImage(this.image, 0, 0);
+            DOMURL.revokeObjectURL(url);
+        }.bind(this)
+        this.image.src = url;
+
+        this.rectangles = [];
+    }
+
+    getRectange(xPx, yPx, widthCells, heightCells) {
+        let x = Math.min(Math.floor(xPx / this.cellSizePx), this.maxWidth - widthCells);
+        let y = Math.min(Math.floor(yPx / this.cellSizePx), this.maxHeight - heightCells);
+        return { 'x': x, 'y': y, 'width': widthCells, 'height': heightCells };
+    }
+
+    drawRectangle(rectangle, color) {
+        // TODO move to getRectangle
+        const { x, y, width, height } = rectangle;
+        this.context.strokeStyle = color;
+        this.context.strokeRect(
+            x * this.cellSizePx + this.strokeWidthPx, y * this.cellSizePx + this.strokeWidthPx,
+            width * this.cellSizePx - this.strokeWidthPx, height * this.cellSizePx - this.strokeWidthPx);
+    }
+
+    drawCurrentBoard() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.drawImage(this.image, 0, 0);
+        for (const rectangle of this.rectangles) {
+            this.drawRectangle(rectangle, 'Red');
+        }
+    }
+
+    addRectangle(rectangle) {
+        this.rectangles.push(rectangle);
+    }
+}
--- js/script.js
+++ js/script.js
@@ -2,87 +2,41 @@
     // Load grid
     'use strict';
 
+    const CELL_SIZE = 20;
+    const STROKE_WIDTH = 1;
+
+    const WIDTH_CELL_COUNT = 18;
+    const HEIGHT_CELL_COUNT = 27;
+
     let canvas = document.getElementById('gameboard');
     let context = canvas.getContext('2d');
-    const cellSize = 20;
-    const strokeWidth = 1;
 
-    const widthCellCount = 18;
-    const heightCellCount = 27;
-    canvas.width = widthCellCount * cellSize + strokeWidth;
-    canvas.height = heightCellCount * cellSize + strokeWidth;
+    let gameBoard = new GameBoard(canvas, context, WIDTH_CELL_COUNT, HEIGHT_CELL_COUNT, CELL_SIZE, STROKE_WIDTH);
 
-    var data = `<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"> \
-        <defs> \
-            <pattern id="grid" width="${cellSize}" height="${cellSize}" patternUnits="userSpaceOnUse"> \
-                <path d="M ${cellSize} 0 L 0 0 0 ${cellSize}" fill="none" stroke="gray" stroke-width="${strokeWidth}" /> \
-            </pattern> \
-        </defs> \
-        <rect width="100%" height="100%" fill="url(#grid)" /> \
-    </svg>`;
-
-    var DOMURL = window.URL || window.webkitURL || window;
-
-    var image = new Image();
-    var svg = new Blob([data], { type: 'image/svg+xml;charset=utf-8' });
-    var url = DOMURL.createObjectURL(svg);
-
-    image.onload = function () {
-        context.drawImage(image, 0, 0);
-        DOMURL.revokeObjectURL(url);
-    }
-    image.src = url;
-
-    // temp put cell code
+    // temp game manager
 
     let tempWidth = 3;
     let tempHeight = 5;
 
-    let rectangles = [];
-
-    function getRectange(mouse_x, mouse_y, widthCells, heightCells) {
-        let x = Math.min(Math.floor(mouse_x / cellSize), widthCellCount - widthCells);
-        let y = Math.min(Math.floor(mouse_y / cellSize), heightCellCount - heightCells);
-        return {'x': x, 'y': y, 'width': widthCells, 'height': heightCells};
-    }
-
-    function drawRectangle(rectangle, color) {
-        // TODO move to getRectangle
-        const {x, y, width, height} = rectangle;
-        context.strokeStyle = color;
-        context.strokeRect(
-            x * cellSize + strokeWidth, y * cellSize + strokeWidth,
-            cellSize * width - strokeWidth, cellSize * height - strokeWidth);
-    }
-
-    function drawCurrentBoard() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(image, 0, 0);
-        for (const rectangle of rectangles) {
-            drawRectangle(rectangle, 'Red');
-        }
-    }
-
     canvas.addEventListener('mousemove', onMouseMove, false);
     function onMouseMove(event) {   
-        context.globalCompositeOperation = "destination-over";
-        drawCurrentBoard();
-        let current_rectangle = getRectange(event.clientX, event.clientY, tempWidth, tempHeight);
-        drawRectangle(current_rectangle, "Red");
+        gameBoard.drawCurrentBoard();
+        let current_rectangle = gameBoard.getRectange(event.clientX, event.clientY, tempWidth, tempHeight);
+        gameBoard.drawRectangle(current_rectangle, "Red");
     }
 
     canvas.addEventListener('click', onMouseClick, false);
     function onMouseClick(event) {
-        let current_rectangle = getRectange(event.clientX, event.clientY, tempWidth, tempHeight);
-        rectangles.push(current_rectangle);
-        drawCurrentBoard();
+        let current_rectangle = gameBoard.getRectange(event.clientX, event.clientY, tempWidth, tempHeight);
+        gameBoard.addRectangle(current_rectangle);
+        gameBoard.drawCurrentBoard();
     }
 
     canvas.addEventListener('contextmenu', function(event) {
         [tempWidth, tempHeight] = [tempHeight, tempWidth];
         event.preventDefault();
-        drawCurrentBoard();
+        gameBoard.drawCurrentBoard();
         let current_rectangle = getRectange(event.clientX, event.clientY, tempWidth, tempHeight);
-        drawRectangle(current_rectangle, "Red");
+        gameBoard.drawRectangle(current_rectangle, "Red");
     });
 })();
